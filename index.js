2ab17e3d0f74f4059c34a148b426a8dc1afbf39b
Paige <paigeberliner@gmail.com>
2023-08-22 17:18:51 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email){
+   return fetch('http://localhost:3000/users' , 
+   {
+    method: 'POST',
+    headers: { 
+
+    },
+
+    })
+    .then (res => res.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    });
+}
+
+
